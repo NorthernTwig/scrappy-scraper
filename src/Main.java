938d671621e589457ab7205daa78b36d20a74733
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-01 10:49:13 +0100

    Initialized

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ÅH JAG VET VAD JAG VILL");
+    }
+}
---
+++ src/Scraper.java
@@ -0,0 +1,13 @@
+public class Scraper {
+    public void initialize() {
+        try {
+            scrape();
+        } catch(Error err) {
+            System.out.println("Something went horribly wrong");
+        }
+    }
+
+    private void scrape() {
+
+    }
+}
